ef5d59059da4b7ee069987293000379a70c8237f
yuga77 <y-n7c8@i.softbank.jp>
2020-02-13 15:11:27 +0900

    s3 onTheWay

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,10 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+server '18.180.21.98', user: 'ec2-user', roles: %w{app db web}
 
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 
 # role-based syntax
 # ==================
@@ -59,4 +62,3 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '18.180.21.98', user: 'ec2-user', roles: %w{app db web}
