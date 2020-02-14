45233c0505152225e8241656a725cab78573abe2
yuga77 <y-n7c8@i.softbank.jp>
2020-02-14 16:48:08 +0900

    valid with all

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 describe User do
   describe '#create' do
 
+    # 1. 全ての項目が存在すれば登録できること
+    it "is valid with all" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
     it "is invalid without a name" do
       user = build(:user, name: "")
       user.valid?
@@ -13,6 +19,6 @@ describe User do
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
-    
+
   end
 end
