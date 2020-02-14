370498ab5b5c81e7016bb4aade2d116ecb4efa6f
yuga77 <y-n7c8@i.softbank.jp>
2020-02-14 18:47:02 +0900

    add email password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,5 +36,27 @@ describe User do
       expect(user.errors[:password_confirmation]).to include("doesn't match Password")
     end
 
+    # 6. 重複したemailが存在する場合登録できないこと
+    it "is invalid with a duplicate email address" do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end
+
+    # 7. passwordが7文字以上であれば登録できること
+    it "is valid with a password that has more than 7 characters " do
+      user = build(:user, password: "0000000", password_confirmation: "0000000")
+      user.valid?
+      expect(user).to be_valid
+    end
+
+    # 8. passwordが6文字以下であれば登録できないこと
+    it "is invalid with a password that has less than 6 characters " do
+      user = build(:user, password: "000000", password_confirmation: "000000")
+      user.valid?
+      expect(user.errors[:password]).to include("is too short (minimum is 7 characters)")
+    end
+
   end
 end
