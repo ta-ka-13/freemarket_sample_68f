6aad95f35563c4d5967f5a11b87403a32b99582d
hatakenji <kenji.0203.generations@docomo.ne.jp>
2020-02-14 15:01:07 +0900

    コミット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,18 @@
 class ItemsController < ApplicationController
   def index
+    @items = item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new
+    if @item.save
+    redirect_to "/item", notice: 商品を登録しました
+    else
+      
   end
 
   def show
