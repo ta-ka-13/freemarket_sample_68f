2d28c96305bbd990b55d46088966d47c68900d5d
yuga77 <y-n7c8@i.softbank.jp>
2020-02-14 19:53:15 +0900

    edit

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,5 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
-  validates :destination_family_name, :destination_last_name, :destination_family_name_kana, :destination_last_name_kana, :postal_code, :prefectures, :municipalities, :address,presence: true
+  validates :destination_family_name, :destination_last_name, :destination_family_name_kana, :destination_last_name_kana, :postal_code, :prefectures, :municipalities, :address, presence: true
   validates :phone_number, uniqueness: true
 end
--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     destination_last_name         {"浩己"}
     destination_family_name_kana  {"アベ"}
     destination_last_name_kana    {"コウキ"}
+    postal_code                   {"2848838"}
     prefectures                   {"千葉県"}
     municipalities                {"船橋市"}
     address                       {"行徳1-3-1"}
