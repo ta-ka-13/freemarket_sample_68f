42955e42fba508681df691c8b9c658147620021c
yuga77 <y-n7c8@i.softbank.jp>
2020-02-14 15:24:28 +0900

    use factory_bot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,8 @@ describe User do
   describe '#create' do
     it "is invalid without a name" do
       user = User.new(name: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
-     user.valid?
-     expect(user.errors[:name]).to include("can't be blank")
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
     end
   end
 end
