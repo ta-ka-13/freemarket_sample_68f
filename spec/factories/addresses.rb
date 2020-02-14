45ad17e30e5b74035e3555d5ee6d094015836cbc
yuga77 <y-n7c8@i.softbank.jp>
2020-02-14 18:31:11 +0900

    return addresses.rb

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,11 +1,11 @@
-# FactoryBot.define do
-#   factory :address do
-#     destination_family_name       {"阿部"}
-#     destination_last_name         {"浩己"}
-#     destination_family_name_kana  {"アベ"}
-#     destination_last_name_kana    {"コウキ"}
-#     prefectures                   {"千葉県"}
-#     municipalities                {"船橋市"}
-#     address                       {"行徳1-3-1"}
-#   end
-# end
+FactoryBot.define do
+  factory :address do
+    destination_family_name       {"阿部"}
+    destination_last_name         {"浩己"}
+    destination_family_name_kana  {"アベ"}
+    destination_last_name_kana    {"コウキ"}
+    prefectures                   {"千葉県"}
+    municipalities                {"船橋市"}
+    address                       {"行徳1-3-1"}
+  end
+end
