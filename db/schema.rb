a2969c3aeec2adefa508db75f71ec81b0c0aa56e
hatakenji <kenji.0203.generations@docomo.ne.jp>
2020-02-14 21:07:26 +0900

    up commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_13_105423) do
+ActiveRecord::Schema.define(version: 2020_02_14_060021) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
