dd92351b5355311d9fdc08013718bf409824ba49
ta-ka-13 <edll5505d@yahoo.co.jp>
2020-02-17 15:37:44 +0900

    conflict_routes_復活

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
+  root "items#index"
+  resources :items do
+    collection do
+      get 'purchase_confirmation'
+    end
+  end
+  resources :categories, only: [:index, :show]
 
 end
