617bcb0944d01456230940814124c6b3f0039639
yoshimaru-ak <59866256+yoshimaru-ak@users.noreply.github.com>
2020-02-18 16:46:59 +0900

    ブランド一覧作成

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,12 +1,5 @@
 class BrandsController < ApplicationController
   def index
-    @hiragana = japanese
     @brands = Brand.all
-  end
-
-  def japanese
-    require 'nkf'
-    hiragana = [*"ｱ".."ﾝ"].map{ |chr| NKF.nkf("-h1w", NKF.nkf("-Xw", chr)) }
-    return hiragana
   end
 end
