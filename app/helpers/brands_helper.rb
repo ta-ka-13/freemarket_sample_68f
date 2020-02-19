f4a93faa3ff71149286ac6906a7f1787462f8bb2
yoshimaru-ak <59866256+yoshimaru-ak@users.noreply.github.com>
2020-02-19 15:48:24 +0900

    brandのパンくず作成

--- app/helpers/brands_helper.rb
+++ app/helpers/brands_helper.rb
@@ -9,4 +9,15 @@ module BrandsHelper
     kana = NKF.nkf("--hiragana -w", brand.name)
     brand_kana = kana.unicode_normalize(:nfd)
   end
+
+  def brandList(brand)
+    nav = [
+      {name: "FURIMA", path: "/"},
+      {name: "ブランド一覧", path: "/brands"}
+    ]
+    unless brand.nil?
+      nav << {name: brand.name, path: "/brands/#{brand.id}"}
+    end
+    nav
+  end
 end
