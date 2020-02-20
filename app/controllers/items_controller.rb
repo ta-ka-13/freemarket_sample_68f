2143b98f40eba8c59c3d61f803133fd77b622aa0
ta-ka-13 <edll5505d@yahoo.co.jp>
2020-02-20 12:10:58 +0900

    コントローラー_ビュー変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,12 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    # 仮置き
+    @item = Item.new
+    @item.images.new
   end
 
   def create
+    
     # 仮置き
     @item = Item.new(item_params)
-
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+    # Item.create(name: item_params[:name], price: item_params[:price], user_id: current_user.id)
   end
 
   def show
@@ -22,13 +31,20 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if items.user_id == current_user.id
+
+    # 出品機能完成したら実装
+    # tweet = Tweet.find(params[:id])
+    # if items.user_id == current_user.id
+    # item.destroy
+    # else
+
+    item = Item.find(1)
+    # if items.user_id == current_user.id
       item.destroy
-    else
-      
-    end
-    
+    # else
+
+    # end
+    redirect_to root_path
   end
 
   def purchase_confirmation
