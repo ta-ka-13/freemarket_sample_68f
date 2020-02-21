382f0099969d59b01a5e838aee46f30382121c87
yuga77 <y-n7c8@i.softbank.jp>
2020-02-21 16:10:10 +0900

    error_messages

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ApplicationRecord
-  belongs_to :item, dependent: :destroy
+  belongs_to :item, dependent: :destroy, optional: true
   mount_uploader :image, ImageUploader
   validates :image, presence: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,9 @@ class Item < ApplicationRecord
   belongs_to :brand, optional: true
   belongs_to :category
   	
-accepts_nested_attributes_for :images, allow_destroy: true
-  validates :name, :price, :description, :condition, :shopping_charges, :shopping_area, :shopping_date, :commission, :profit, presence: true
+  accepts_nested_attributes_for :images, allow_destroy: true
+  validates_associated :images
+  validates :images, :name, :description, :condition, :shopping_charges, :shopping_area, :shopping_date, :price, presence: true
   has_ancestry
   accepts_nested_attributes_for :images, allow_destroy: true
 end
