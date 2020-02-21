1d4b0767085f218a8cbd68f847dab2bc2e2eb1c6
yoshimaru-ak <59866256+yoshimaru-ak@users.noreply.github.com>
2020-02-21 15:26:44 +0900

    商品編集機能(途中)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,10 +11,14 @@ class ItemsController < ApplicationController
   def show
   end
 
-  def update
+  def edit
+    @item = Item.find(params[:id])
+    @images = @item.images
   end
 
-  def edit
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
   end
 
   def destroy
@@ -24,6 +28,15 @@ class ItemsController < ApplicationController
   end
 
   def purchase
+  end
+
+  private
+  def item_params
+    params.require(:item).permit( :name, :price, :description, :condition, :shopping_charges, :shopping_area, :shopping_data, :_id, :buyer, :user_id, :brand_id, :category_id, :created_at, :updated_at, )
+    
+    def update_params
+      params.require(:item).permit( :name, :price, :description, :condition, :shopping_charges, :shopping_area, :shopping_data, :_id, :buyer, :user_id, :brand_id, :category_id, :created_at, :updated_at, )
+    end
   end
   
 end
