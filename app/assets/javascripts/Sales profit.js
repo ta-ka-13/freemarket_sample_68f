379b778b4f41bc4a02ab5ece6389593093baa145
yuga77 <y-n7c8@i.softbank.jp>
2020-02-21 14:03:55 +0900

    Update Sales profit.js

--- app/assets/javascripts/Sales profit.js
+++ app/assets/javascripts/Sales profit.js
@@ -1,6 +1,6 @@
 // 入力された価格に応じて販売手数料と販売利益の計算
 $(function(){
-  $('#price_calc').on('input', function(){ 
+  $(document).on("input", '#price_calc', function(){ 
     var data = $('#price_calc').val(); 
     var profit = Math.round(data * 0.9)  
     var fee = (data - profit) 
