eecc98c1cc7d642de451e1adb611e6f9d83c8351
yuga77 <y-n7c8@i.softbank.jp>
2020-02-23 21:02:56 +0900

    other model test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -26,6 +26,48 @@ RSpec.describe Item, type: :model do
         item.valid?
         expect(item.errors[:name]).to include("を入力してください")
       end
+
+      it 'is invalid without price' do
+        item = build(:item, :item_with_image, price: nil )
+        item.valid?
+        expect(item.errors[:price]).to include("を入力してください")
+      end
+
+      it 'is invalid without description' do
+        item = build(:item, :item_with_image, description: nil )
+        item.valid?
+        expect(item.errors[:description]).to include("を入力してください")
+      end
+
+      it 'is invalid without condition' do
+        item = build(:item, :item_with_image, condition: nil )
+        item.valid?
+        expect(item.errors[:condition]).to include("を入力してください")
+      end
+
+      it 'is invalid without shopping_charges' do
+        item = build(:item, :item_with_image, shopping_charges: nil )
+        item.valid?
+        expect(item.errors[:shopping_charges]).to include("を入力してください")
+      end
+
+      it 'is invalid without shopping_area' do
+        item = build(:item, :item_with_image, shopping_area: nil )
+        item.valid?
+        expect(item.errors[:shopping_area]).to include("を入力してください")
+      end
+
+      it 'is invalid without shopping_date' do
+        item = build(:item, :item_with_image, shopping_date: nil )
+        item.valid?
+        expect(item.errors[:shopping_date]).to include("を入力してください")
+      end
+
+      it 'is invalid without login' do
+        item = build(:item, :item_with_image, user: nil )
+        item.valid?
+        expect(item.errors[:user]).to include("を入力してください")
+      end
     end
   end
 end
