29f1102a7435c08300014bcfdf7f889207866222
yuga77 <y-n7c8@i.softbank.jp>
2020-02-24 20:43:24 +0900

    add brand

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ApplicationRecord
   has_many :images, dependent: :destroy
   belongs_to :user
-  belongs_to :brand, optional: true
   belongs_to :category
   	
   accepts_nested_attributes_for :images, allow_destroy: true
